7d14ac7541edd9e1b464949e58c8ac2ab04d666f
Andrew Ketsko <monicman82@gmail.com>
2023-12-12 16:35:25 +0200

    start

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const { ingress } = require("./ngroksetup");
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+
+app.all("/", (req, res) => {
+  res.json({ message: "test" });
+});
+
+app.post("/webhooks", (req, res) => {
+  let { body, headers } = req;
+  console.log("-------------- New Request POST --------------");
+  console.log("Headers:" + JSON.stringify(headers, null, 3));
+  console.log("Body:" + JSON.stringify(body, null, 3));
+  if (body.object === "page") {
+    console.log("Responding with 200");
+    res.status(200).json({ message: "Thank you for the message" });
+  } else {
+    console.log("Responding with 404");
+    res.sendStatus(404);
+  }
+});
+
+app.get("/webhooks", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+  let { body, headers } = req;
+
+  console.log("-------------- New Request GET --------------");
+  console.log("Headers:" + JSON.stringify(headers, null, 3));
+  console.log("Body:" + JSON.stringify(body, null, 3));
+
+  if (mode && token) {
+    if (mode === "subscribe" && token === "12345") {
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      console.log("Responding with 403 Forbidden");
+      res.sendStatus(403);
+    }
+  } else {
+    console.log("Replying Thank you.");
+    res.json({ message: "Thank you for the message" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("server started on port 3000");
+  ingress();
+});
---
+++ ngroksetup.js
@@ -0,0 +1,16 @@
+const ngrok = require("@ngrok/ngrok");
+
+const ingress = async () => {
+  const listener = await ngrok.forward({
+    addr: 3000,
+    // authtoken_from_env: true,
+    authtoken: process.env.AUTH_TOKEN,
+    domain: process.env.DOMAIN,
+    // verify_webhook_provider: "instagram",
+    // verify_webhook_secret: "{some secret}",
+  });
+
+  console.log(`available at: ${listener.url()}`);
+};
+
+module.exports = { ingress };
